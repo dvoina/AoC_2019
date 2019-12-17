543c5e088e7ee200b94b19c7cf0b7f6b650e3214
Daniel Voina <nilu@HAL.lan>
2019-12-17 20:08:04 +0200

    Day 16 (part 2)

--- d16.py
+++ d16.py
@@ -1,30 +1,40 @@
 baseSeq = (0,1,0,-1)
 if __name__=="__main__":
-    n = [int(d) for d in open("d16.in").readline().strip()[:]]
+    l = open("d16.in").readline().strip()[:]
+    skip = int("".join(l[:7]))
+    # n = l
+    # N = len(n)
+    # seq = {}
+    # for i in range(1,N+1):
+    #     s = []
+    #     for j in baseSeq:
+    #         s.extend([j] * i)
+    #     seq[i-1] = s[:]
 
+    # for p in range(100):
+    #     v = []
+    #     for i in range(N):
+    #         _v = 0
+    #         _s = seq[i]
 
-    N = len(n)
-    seq = {}
-    for i in range(1,N+1):
-        s = []
-        for j in baseSeq:
-            s.extend([j] * i)
-        seq[i-1] = s[:]
-    for p in range(100):
-        v = []
-        for i in range(N):
-            _v = 0
-            _s = seq[i]
+    #         _n = len(_s)
+    #         for i,_d in enumerate(n):
+    #             x = _s[(i+1)%_n]
+    #             _v += int(_d)*x
+    #         v.append(str(_v)[-1])
+    #     # print(v)
+    #     n = v
 
-            _n = len(_s)
-            for i,_d in enumerate(n):
-                x = _s[(i+1)%_n]
-                _v += _d*x
-            v.append(abs(_v)%10)
-        # print(v)
-        n = v
-    print("".join(n[:8]))
-        
+    # print("".join(n[:8]))
+    
+    ll = list(map(int, l))
+    ll *= 10000
+
+    for _ in range(100):
+        for i in reversed(range(skip, len(ll)-1)):
+            ll[i] = (ll[i] + ll[i+1]) % 10
+
+    print(ll[skip:(skip+8)])
             
 
 
